8a37f7308a13a0c394564ed14f3457b856a92acd
nicklegr <1327502+nicklegr@users.noreply.github.com>
2023-07-20 13:58:40 +0900

    行番号も取得

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-use std::fs;
+use std::{fs, io};
+
+struct MatchLine {
+    line_no: usize,
+    content: String,
+}
 
 fn main() {
     process_file("playlist.m3u8").unwrap();
 }
 
-fn process_file(file_name: &str) -> std::io::Result<Vec<String>> {
+fn process_file(file_name: &str) -> io::Result<Vec<MatchLine>> {
     let content = fs::read_to_string(file_name)?;
 
-    let match_lines: Vec<&str> =
+    let match_lines: Vec<MatchLine> =
         content.split("\n")
-        // .enumerate()
-        .filter(|x| x.contains(".ts"))
+        .enumerate()
+        .filter(|x| x.1.contains(".ts"))
+        .map(|x| MatchLine { line_no: x.0 + 1, content: String::from(x.1) })
         .collect();
 
     for line in &match_lines {
-        println!("{}", line);
+        println!("{}: {}", line.line_no, line.content);
     }
 
-    Ok(vec![String::from("test")])
+    Ok(match_lines)
 }
