04b1c3436a6caa8a88b4599b8e1e19e29941966b
nicklegr <1327502+nicklegr@users.noreply.github.com>
2023-07-20 10:02:59 +0900

    マッチした行を表示

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    process_file("playlist.m3u8").unwrap();
+}
+
+fn process_file(file_name: &str) -> std::io::Result<Vec<String>> {
+    let content = fs::read_to_string(file_name)?;
+
+    let match_lines: Vec<&str> =
+        content.split("\n")
+        // .enumerate()
+        .filter(|&x| x.contains(".ts"))
+        .collect();
+
+    for line in match_lines {
+        println!("{}", line);
+    }
+
+    Ok(vec![String::from("test")])
 }
