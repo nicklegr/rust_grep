5bac4274f3dc2e4d49d2dcd5493ec66fd723da00
nicklegr <1327502+nicklegr@users.noreply.github.com>
2023-07-26 14:14:16 +0900

    async/await版も書いてみた

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{fs, io, path, sync::mpsc};
+use std::{fs, io, path, sync::mpsc, result};
 use threadpool::ThreadPool;
+use tokio::task::JoinHandle;
 
 struct FileResult {
     file_name: String,
@@ -11,7 +12,8 @@ struct MatchLine {
     content: String,
 }
 
-fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     // iterator of Result<T, E> items can be collected into Result<Collection<T>, E>
     let paths =
         fs::read_dir("texts")?
@@ -24,7 +26,8 @@ fn main() -> io::Result<()> {
     //     .map(|e| e.path())
     //     .collect();
 
-    process_files(paths)
+    // process_files(paths)
+    process_files_async(paths).await
 }
 
 fn process_files(paths: Vec<path::PathBuf>) -> io::Result<()> {
@@ -60,6 +63,39 @@ fn process_files(paths: Vec<path::PathBuf>) -> io::Result<()> {
 
 fn process_file(path: &path::Path) -> io::Result<Vec<MatchLine>> {
     let content = fs::read_to_string(path)?;
+
+    let match_lines: Vec<MatchLine> =
+        content.lines()
+        .enumerate()
+        .filter(|x| x.1.contains("def "))
+        .map(|x| MatchLine { line_no: x.0 + 1, content: String::from(x.1) })
+        .collect();
+
+    Ok(match_lines)
+}
+
+async fn process_files_async(paths: Vec<path::PathBuf>) -> io::Result<()> {
+    let handles: Vec<JoinHandle<FileResult>> =
+        paths.into_iter().map(|path|
+            tokio::spawn(async move {
+                let file_name = path.file_name().unwrap().to_str().unwrap();
+                let match_lines = process_file_async(&path).await.unwrap();
+                FileResult { file_name: file_name.to_string(), match_lines }
+            })
+        ).collect();
+
+    for handle in handles {
+        let result = handle.await.unwrap();
+        for line in result.match_lines {
+            println!("{}:{}: {}", result.file_name, line.line_no, line.content);
+        }
+    }
+
+    Ok(())
+}
+
+async fn process_file_async(path: &path::Path) -> io::Result<Vec<MatchLine>> {
+    let content = tokio::fs::read_to_string(path).await?;
 
     let match_lines: Vec<MatchLine> =
         content.lines()
